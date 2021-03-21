56a84146c594c12cad6d547d4ed8478e5c88b041
Riku Eskelinen <78163860+riku614@users.noreply.github.com>
2021-03-21 22:21:34 +0200

    Add files via upload

--- tf2_harjoitus/src/turtle_tf2_broadcaster.py
+++ tf2_harjoitus/src/turtle_tf2_broadcaster.py
@@ -11,10 +11,10 @@ def handle_turtle_pose(msg, turtlename):
     transform = geometry_msgs.msg.TransformStamped()
 
     transform.header.stamp = rospy.Time.now()
-    transform.header.stamp_id = "world"
+    transform.header.frame_id = "world"
     transform.child_frame_id = turtlename
-    transform.transform.translation.x =msg.x
-    transform.transform.translation.y =msg.y
+    transform.transform.translation.x = msg.x
+    transform.transform.translation.y = msg.y
     transform.transform.translation.z = 0.0
     quaternion = tf_conversions.transformations.quaternion_from_euler(0,0,msg.theta)
     transform.transform.rotation.x =quaternion[0]
@@ -23,13 +23,13 @@ def handle_turtle_pose(msg, turtlename):
     transform.transform.rotation.w =quaternion[3]
 
     br.sendTransform(transform)
+    
 
-
-if __name__ == '__main__ :
-    rospy.init_node('tf2 turtle_broadcaster')
+if __name__ == '__main__':
+    rospy.init_node('tf2_turtle_broadcaster')
     turtlename = rospy.get_param('~turtle')
     rospy.Subscriber('/%s/pose' % turtlename,
                     Pose,
                     handle_turtle_pose,
                     turtlename)
-    rospy.spin()   
+    rospy.spin()           
